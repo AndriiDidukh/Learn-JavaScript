e80a2f071afb250d68edef3e5eda3aeae7dcd2f4
Andrii Didukh <andriididukh@gmail.com>
2024-03-16 18:55:40 +0200

    completed chapters 3 - 4.1

---
+++ Chapter 3/3.5 Automated testing with Mocha /javascript.js
@@ -0,0 +1,19 @@
+/* 
+Що не так з тестом функціій pow, вказаним нижче?
+P.S. Синтаксичних помилок не має і тести проходять.
+
+it("Підносить x до n-нного степеня", function() {
+  let x = 5;
+
+  let result = x;
+  assert.equal(pow(x, 1), result);
+
+  result *= x;
+  assert.equal(pow(x, 2), result);
+
+  result *= x;
+  assert.equal(pow(x, 3), result);
+});
+ */
+
+// Помилкова логіка створення тесту
